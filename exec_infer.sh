f3e178e6479c811d884f5a49e88703670b36d7a1
Tran Dong <phuongdong12112001@gmail.com>
2023-07-15 17:07:29 +0700

    third commit

--- exec_infer.sh
+++ exec_infer.sh
@@ -1,7 +1,7 @@
-python /kaggle/working/Chatbot_multi_turn/src/main.py \
+python /content/Chatbot_multi_turn/src/main.py \
     --seed=0 \
     --mode="infer" \
-    --data_dir="/kaggle/working/data" \
+    --data_dir="/content/data" \
     --model_type="gpt2" \
     --bos_token="<bos>" \
     --sp1_token="<sp1>" \
@@ -10,6 +10,6 @@ python /kaggle/working/Chatbot_multi_turn/src/main.py \
     --max_len=1024 \
     --max_turns=5 \
     --top_p=0.8 \
-    --ckpt_dir="/kaggle/working/saved_models" \
-    --ckpt_name="best_ckpt_epoch=1_valid_loss=2.8172" \
+    --ckpt_dir="/content/drive/MyDrive" \
+    --ckpt_name="best_ckpt_epoch=3_valid_loss=2.6681" \
     --end_command="Abort!"
--- exec_load_data.sh
+++ exec_load_data.sh
@@ -1,5 +1,5 @@
-python /kaggle/working/Chatbot_multi_turn/src/load_data.py \
-    --data_dir="/kaggle/working/data" \
+python /content/Chatbot_multi_turn/src/load_data.py \
+    --data_dir="/content/data" \
     --train_prefix="train" \
     --valid_prefix="valid" \
     --train_frac=0.85 \
